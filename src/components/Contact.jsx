c61f2959a43f0f93093ae0459b5f5a2ffb70eb8c
Sameer <sameerninja009@gmail.com>
2026-07-08 16:56:08 +0500

    Improve contact section and fix certificate images

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,6 @@
 import {useRef} from "react";
-import emailjs from "@emailjs/browser"; 
+import emailjs from "@emailjs/browser";
+import { FaEnvelope } from "react-icons/fa"; 
 
 function Contact(){
 
@@ -17,6 +18,7 @@ function Contact(){
     .then(
       ()=>{
         alert("Message sent successfully!");
+        form.current.reset();
       },
       (error)=>{
         alert("Failed to send:"+error.text)
@@ -27,7 +29,14 @@ function Contact(){
   return(
     <section id="contact">
       <h2>Contact Me</h2>
-      <a href="">Email: sameerninja009@gmail.com</a>
+      <a
+  href="https://mail.google.com/mail/?view=cm&fs=1&to=sameerninja009@gmail.com"
+  className="email-link" target="_blank"
+  rel="noopener noreferrer"
+>
+  <FaEnvelope />
+   sameerninja009@gmail.com
+</a>
       <form ref={form} onSubmit={sendEmail}>
         <input type="text" name="user_name" placeholder="Your Name" required />
         <input type="email" name="user_email" placeholder="Your Email" required />
